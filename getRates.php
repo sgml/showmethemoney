52fcba099fe1c2662b86783b9d59269f481aba75
sgml <paul.sweatte@etrade.com>
2013-03-16 08:53:06 -0700

    Update getRates.php
    Added TODO for loading XML into table

--- getRates.php
+++ getRates.php
@@ -3,8 +3,12 @@ function getRates($service)
   {
   $rates_file = '/tmp/rates.xml';
   $this->rates = file_get_contents($service);
-  file_put_contents($rates_file, $rates);
-  LOAD XML LOCAL INFILE '/tmp/rates.xml' INTO TABLE forex ROWS IDENTIFIED BY '<conversion>';
+  file_put_contents($rates_file, $this->rates);
+  // TODO: Validate XML content type and load into table 
+  /*
+  $xml = "LOAD XML LOCAL INFILE '/tmp/rates.xml' INTO TABLE ('forex') ROWS IDENTIFIED BY '<conversion>';
+  mysql_query($xml);
+  */
   parseData();
   }
 ?>
