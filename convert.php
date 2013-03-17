7bc4b0f536b9a65a492c63d58cab75510158358c
sgml <paul.sweatte@etrade.com>
2013-03-17 08:20:03 -0700

    Update convert.php
    Added return statement

--- convert.php
+++ convert.php
@@ -3,6 +3,6 @@ function convert($data)
   {
   $num = filter_var($data, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
   $locale = rtrim(str_replace($num, "", $data));
-  lookup($locale, $num);
+  return lookup($locale, $num);
   }
 ?>
