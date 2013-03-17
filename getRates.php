7aa57b9b696b983d74bad92691e15702e830e340
sgml <paul.sweatte@etrade.com>
2013-03-17 08:33:28 -0700

    Update getRates.php
    Updated file path and scope

--- getRates.php
+++ getRates.php
@@ -1,9 +1,9 @@
 <?php
 function getRates($service)
   {
-  $rates_file = '/tmp/rates.xml';
-  $this->rates = file_get_contents($service);
-  file_put_contents($rates_file, $this->rates);
+  $rates_file = 'rates.xml';
+  $rates = file_get_contents($service);
+  file_put_contents($rates_file, $rates);
   // TODO: Validate XML content type and load into table 
   /*
   $xml = "LOAD XML LOCAL INFILE '/tmp/rates.xml' INTO TABLE ('forex') ROWS IDENTIFIED BY '<conversion>';
