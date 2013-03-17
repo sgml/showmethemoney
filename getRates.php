3c3bff88b42d7e9e0f3bd57b11296f43dad45c98
sgml <paul.sweatte@etrade.com>
2013-03-17 08:45:28 -0700

    Update getRates.php
    Changed scope and added test

--- getRates.php
+++ getRates.php
@@ -1,7 +1,7 @@
-<?php
 function getRates($service)
   {
-  $rates_file = 'rates.xml';
+  $dir = dirname( __FILE__ );
+  $rates_file = "$dir/rates.xml";
   $rates = file_get_contents($service);
   file_put_contents($rates_file, $rates);
   // TODO: Validate XML content type and load into table 
@@ -9,6 +9,7 @@ function getRates($service)
   $xml = "LOAD XML LOCAL INFILE '/tmp/rates.xml' INTO TABLE ('forex') ROWS IDENTIFIED BY '<conversion>';
   mysql_query($xml);
   */
-  parseData();
+  //parseData();
   }
-?>
+
+getRates('http://toolserver.org/~kaldari/rates.xml');
